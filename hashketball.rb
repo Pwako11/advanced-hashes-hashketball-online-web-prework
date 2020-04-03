53db852c1c4089b2db66871f8fa2f9e543e4547d
Patrick A <akolo@hotmail.com>
2020-04-03 04:19:29 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -193,7 +193,7 @@ def player_stats(player_name)
   stats = load_players.find do |names|
     names[:player_name] == player_name
   end
-  
-(stats)1..-1
+range1 = 1..8
+range1.stats()
   binding.pry
 end 
