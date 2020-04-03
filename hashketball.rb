2129876677a12b74fc1d407fbca3df95340e70c0
Patrick A <akolo@hotmail.com>
2020-04-03 02:02:37 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -173,11 +173,11 @@ def team_names
 end 
 
 def player_numbers(team)
-  selected_team = game_hash.keys.map do |t|
-    if game_hash[t][:team_name] == team
-      game_hash[t][:team_name][:number]
-    end
-  end  
-  selected_team.flatten
+  # selected_team = game_hash.keys.map do |t|
+  #   if game_hash[t][:team_name] == team
+  #     game_hash[t][:team_name][:number]
+  #   end
+  # end  
+  # selected_team.flatten
   binding.pry
 end
